6fa3f54517d6b20a31a2d729c5bd2cbb26df90e9
i12sleep <z5477108@ad.unsw.edu.au>
2023-10-27 16:49:58 +1100

    setup development environment

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
-import print;
+//import print;
+#include <iostream>
 
-main() -> int {
-	std::println("Hello World!");
+//main() -> int {
+	//std::println("Hello World!");
+//}
+
+int main() {
+	initVulkan();
+	return 0;
 }
 
+void initVulkan() {
+	createInstance();
+}
